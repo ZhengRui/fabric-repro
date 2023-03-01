49239bd55c0fa407a327a569b7b25c274dec867c
Rui <rzhengphy@gmail.com>
2023-03-01 15:47:08 +0800

    finish the objectmodified demo

--- src/app/objectmodified/page.tsx
+++ src/app/objectmodified/page.tsx
@@ -6,6 +6,8 @@ import * as fabric from "fabric";
 const ObjectModifiedDemo = () => {
   const canvasElmRef = useRef<HTMLCanvasElement | null>(null);
   const canvasRef = useRef<fabric.Canvas | null>(null);
+  const redBoxRef = useRef<fabric.Rect | null>(null);
+  const greenBoxRef = useRef<fabric.Rect | null>(null);
 
   const initialized = useRef(false);
 
@@ -23,40 +25,44 @@ const ObjectModifiedDemo = () => {
     extendElm.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
 
     // add some boxes
-    canvas.add(
-      new fabric.Rect({
-        left: 50,
-        top: 50,
-        width: 200,
-        height: 200,
-        stroke: "red",
-        strokeWidth: 2,
-        fill: "rgba(255, 0, 0, 0)",
-        perPixelTargetFind: true,
-      }),
+    const redBox = new fabric.Rect({
+      left: 50,
+      top: 50,
+      width: 200,
+      height: 200,
+      stroke: "red",
+      strokeWidth: 2,
+      fill: "rgba(255, 0, 0, 0)",
+      perPixelTargetFind: true,
+    });
 
-      new fabric.Rect({
-        left: 300,
-        top: 200,
-        width: 150,
-        height: 200,
-        stroke: "green",
-        strokeWidth: 2,
-        fill: "rgba(255, 0, 0, 0)",
-        perPixelTargetFind: true,
-      })
-    );
+    const greenBox = new fabric.Rect({
+      left: 300,
+      top: 200,
+      width: 150,
+      height: 200,
+      stroke: "green",
+      strokeWidth: 2,
+      fill: "rgba(255, 0, 0, 0)",
+      perPixelTargetFind: true,
+    });
+
+    canvas.add(redBox, greenBox);
 
     canvas.on("object:modified", (e) => {
-      console.log("object:modified called");
+      alert("object:modified called");
 
-      //   const objects = canvas.getObjects();
-      //   canvas.remove(...objects);
-      //   canvas.requestRenderAll();
+      if (e.target === greenBox) {
+        const objects = canvas.getObjects();
+        canvas.remove(...objects);
+      }
+      canvas.requestRenderAll();
     });
 
     initialized.current = true;
     canvasRef.current = canvas;
+    redBoxRef.current = redBox;
+    greenBoxRef.current = greenBox;
   };
 
   useEffect(initCanvas, [canvasElmRef]);
@@ -80,6 +86,32 @@ const ObjectModifiedDemo = () => {
         >
           delete all objects
         </button>
+
+        <button
+          style={{
+            background: "cyan",
+            border: 0,
+            padding: 10,
+            width: 100,
+            marginLeft: 20,
+          }}
+          onClick={() => {
+            const canvas = canvasRef.current;
+            if (!canvas || canvas.getObjects().length) return;
+
+            canvas.add(redBoxRef.current!, greenBoxRef.current!);
+            canvas.requestRenderAll();
+          }}
+        >
+          reset
+        </button>
+      </div>
+      <div> delete all objects does not trigger object:modified</div>
+      <div> move red box triggers object:modified once</div>
+      <div>
+        {" "}
+        move green box triggers object:modified twice because delete all objects
+        inside its callback
       </div>
     </div>
   );
